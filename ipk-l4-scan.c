b5ee703977ca9f8545fc7c2c7f721d218b53839e
xtomasp00 <patatoma@seznam.cz>
2025-03-03 23:38:28 +0100

    Started work on the packet construction and checksum calculation. The main goal is not to connect it together and also convert the IP addresses to the correct formats (not strings).

--- ipk-l4-scan.c
+++ ipk-l4-scan.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <stdint.h>
 #include <ctype.h>
 #include <string.h>
 #include <netdb.h>
@@ -10,10 +11,13 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <netinet/udp.h>
+#include <netinet/tcp.h>
 
 #define DEFAULT_TIMEOUT 5000 //the default timeout if the user doesn't specify otherwise
 #define DOMAIN_REGEX "^([a-zA-Z0-9][-a-zA-Z0-9]*\\.)+[a-zA-Z]{2,}$" //regex used to verify wether user entered a valid domain name
 #define MAX_PORTS 65535 //maximum ammount of possible ports to scan through
+#define SOURCE_PORT 50000 //source port, it is in the ephemeral port range, and i just decided for it because it was nice
 
 typedef enum {
     TYPE_UNKNOWN,
@@ -22,19 +26,175 @@ typedef enum {
     TYPE_DOMAIN
 } target_type;
 
-void scan_tcp_ipv4(char* target, int* ports_array) {
-        printf("Inside scan_tcp_ipv4 -> Target: %s\n", target);
+struct pseudo_header_ipv4{
+        uint32_t source_ip;
+        uint32_t destination_ip;
+        uint8_t zero;
+        uint8_t protocol;
+        uint16_t udp_tcp_length;
+};
+
+struct pseudo_header_ipv6{
+        uint8_t source_ip[16];
+        uint8_t destination_ip[16];
+        uint8_t zero;
+        uint8_t protocol;
+        uint16_t udp_tcp_length;
+};
+
+uint16_t sum_calculator(void *data, int length){
+        uint32_t sum = 0;
+        uint16_t odd_byte = 0;
+        uint16_t *pointer = (uint16_t *)data;
+        
+        while(length > 1){
+                sum += *pointer++;
+                length=-2;
+        }
+        
+        if (length == 1){
+                *(unsigned char *)(&odd_byte) = *(unsigned char *)pointer;
+                sum += odd_byte;
+        }
+        
+        sum =(sum >> 16) + (sum & 0xFFFF);
+        sum +=(sum >> 16);
+        return ~sum;
 }
 
-void scan_tcp_ipv6(char* target, int* ports_array) {
+uint16_t udp_checksum(void* source_ip, void* dest_ip, struct udphdr* udp_header, int udp_length, bool ipv6_mode){
+        uint16_t* buffer;
+        int final_length;
+        
+        if(ipv6_mode){
+                struct pseudo_header_ipv4 p_header;
+                memcpy(p_header.source_ip, source_ip, 16);
+                memcpy(p_header.dest_ip, dest_ip, 16);
+                p_header.reserved = 0;
+                p_header.protocol = IPPROTO_UDP;
+                p_header.udp_length = htons(udp_len);
+                
+                total_len = sizeof(struct pseudo_header_ipv6) + udp_len;
+                buffer = malloc(total_len);
+                
+                memcpy(buffer, &p_header, sizeof(struct pseudo_header_ipv6));
+                memcpy(buffer + sizeof(struct pseudo_header_ipv6) / 2, udp_header, udp_len);
+        }else{
+                struct pseudo_header_ipv6 p_header;
+                p_header.source_ip = *(uint32_t *)source_ip;
+                p_header.dest_ip = *(uint32_t *)dest_ip;
+                p_header.reserved = 0;
+                p_header.protocol = IPPROTO_UDP;
+                p_header.udp_length = htons(udp_len);
+                
+                total_len = sizeof(struct pseudo_header_ipv4) + udp_len;
+                buffer = malloc(total_len);
+                
+                memcpy(buffer, &p_header, sizeof(struct pseudo_header_ipv4));
+                memcpy(buffer + sizeof(struct pseudo_header_ipv4) / 2, udp_header, udp_len);
+        }
+        
+        uint16_t checksum_value = checksum(buffer, total_len);
+        free(buffer);
+        
+        return checksum_value;
+}
+
+uint16_t tcp_checksum(void* source_ip, void* dest_ip, struct tcphdr* tcp_header, int tcp_length, bool ipv6_mode){
+        uint16_t* buffer;
+        int final_length;
+        
+        if(ipv6_mode){
+                struct pseudo_header_ipv4 p_header;
+                memcpy(p_header.source_ip, source_ip, 16);
+                memcpy(p_header.dest_ip, dest_ip, 16);
+                p_header.reserved = 0;
+                p_header.protocol = IPPROTO_TCP;
+                p_header.tcp_length = htons(tcp_len);
+                
+                total_len = sizeof(struct pseudo_header_ipv6) + tcp_len;
+                buffer = malloc(total_len);
+                
+                memcpy(buffer, &p_header, sizeof(struct pseudo_header_ipv6));
+                memcpy(buffer + sizeof(struct pseudo_header_ipv6) / 2, tcp_header, tcp_len);
+        }else{
+                struct pseudo_header_ipv6 p_header;
+                p_header.source_ip = *(uint32_t *)source_ip;
+                p_header.dest_ip = *(uint32_t *)dest_ip;
+                p_header.reserved = 0;
+                p_header.protocol = IPPROTO_TCP;
+                p_header.tcp_length = htons(udp_len);
+                
+                total_len = sizeof(struct pseudo_header_ipv4) + tcp_len;
+                buffer = malloc(total_len);
+                
+                memcpy(buffer, &p_header, sizeof(struct pseudo_header_ipv4));
+                memcpy(buffer + sizeof(struct pseudo_header_ipv4) / 2, tcp_header, tcp_len);
+        }
+        
+        uint16_t checksum_value = checksum(buffer, total_len);
+        free(buffer);
+        
+        return checksum_value;
+}
+
+void construct_tcp_header(void* target_ip, uint16_t destination_port, struct tcphdr* header, bool ipv6_mode, void* source_ip){
+        memset(header, 0, sizeof(struct tcphder));
+        
+        header->th_sport = htons(SOURCE_PORT);
+        header->th_dport = htons(destination_port);
+        header->th_seq = htonl(rand());
+        header->th_flags = TH_SYN;
+        header->th_off = 5;
+        header->th_win = htons(65535);
+        tcp->th_sum = 0;
+        
+        uint16_t sum = tcp_checksum(source_ip, target_ip, header, sizeof(struct tcphdr), ipv6_mode);
+        header->th_sum = sum;
+        
+        return ;
+}
+
+void construct_udp_header(void* target_ip, uint16_t destination_port, struct udphdr* header, bool ipv6_mode, void* source_ip){
+        memset(header, 0, sizeof(struct udphder));
+        
+        header->uh_sport = htons(SOURCE_PORT);
+        header->uh_dport = htons(destination_port);
+        header->uh_ulen = htons(sizeof(struct udphdr));
+        tcp->uh_sum = 0;
+        
+        uint16_t checksum = udp_checksum(source_ip, target_ip, header, sizeof(struct udphdr), ipv6_mode);
+        header->uh_sum = checksum;
+        
+        return ;
+}
+
+int raw_socket_maker(int protocol){
+        int socket_var = socket(AF_INET, SOCK_RAW, protocol);
+        if(socket_var < 0){
+                fprintf(stderr, "Error: The creation of a raw socket failed.");
+                return -1;
+        }
+        
+}
+
+void scan_tcp_ipv4(uint32_t target, int* ports_array) {
+        for(int index = 0; index < MAX_PORTS; index++){
+                if(ports_array[index]){
+                        
+                }
+        }
+}
+
+void scan_tcp_ipv6(struct in6_addr target, int* ports_array) {
         printf("Inside scan_tcp_ipv6 -> Target: %s\n", target);
 }
 
-void scan_udp_ipv4(char* target, int* ports_array) {
+void scan_udp_ipv4(uint32_t target, int* ports_array) {
         printf("Inside scan_udp_ipv4 -> Target: %s\n", target);
 }
 
-void scan_udp_ipv6(char* target, int* ports_array) {
+void scan_udp_ipv6(struct in6_addr, int* ports_array) {
         printf("Inside scan_udp_ipv6 -> Target: %s\n", target);
 }
 
@@ -282,20 +442,31 @@ int main(int argc, char *argv[]){
 	int udp_ports[MAX_PORTS] = {0};
 	
 	if(!get_input_params(argc, argv, &interface, &timeout, &target, &udp_ports_string, &tcp_ports_string) && interface){
-		printf("%s %s %d \n", interface, target, timeout);
+		return 1;
 	}
 	target_type ttype = determine_target_type(target);
+	
 	if(ttype == TYPE_UNKNOWN){
 	        fprintf(stderr, "Error: The specified target is not correct.");
 	        return 1;
 	}else if(ttype == TYPE_DOMAIN){
 	        iterate_domain_ips(target, tcp_ports, udp_ports);
 	}else if(ttype == TYPE_IPV4){
-	        scan_tcp_ipv4(target, tcp_ports);
-                scan_udp_ipv4(target, udp_ports);
+	        uint32_t converted_ipv4;
+	        
+	        if(inet_pton(AF_INET, target, &converted_ipv4) != 1)
+	                fprintf(stderr, "Error: An error occured converting the target IP address to uint32_t.");
+	        
+	        scan_tcp_ipv4(converted_ipv4, tcp_ports);
+                scan_udp_ipv4(converted_ipv4, udp_ports);
 	}else{
-	        scan_tcp_ipv6(target, tcp_ports);
-                scan_udp_ipv6(target, udp_ports);
+	        struct ip6_addr converted_ipv6;
+	        
+	        if(inet_pton(AF_INET6, target, &converted_ipv4) != 1)
+	                fprintf(stderr, "Error: An error occured converting the target IP address to uint32_t.");
+	                
+	        scan_tcp_ipv6(converted_ipv6, tcp_ports);
+                scan_udp_ipv6(converted_ipv6, udp_ports);
 	}
 	return 0;
 }
